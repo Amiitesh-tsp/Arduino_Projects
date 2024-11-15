a86f3ae524f215cec07509270d70475c15689cc6
Amiitesh TSP <tspamiitesh@gmail.com>
2024-11-15 11:56:52 +0530

    Create temp_ppm.cpp

---
+++ temp_ppm.cpp
@@ -0,0 +1,32 @@
+e <DHT11.h>
+
+DHT11 dht(2);
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  int sensorValue = analogRead(A0);
+  float voltage = sensorValue * (5.0 / 1023.0);
+  float ppm = 11.7 * pow(voltage, -1.67);
+
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  Serial.print("Humidity: ");
+  Serial.print(h);
+  Serial.print(" %\t");
+  Serial.print("Temperature: ");
+  Serial.print(t);
+  Serial.println(" *C ");
+
+  Serial.print("Air Pollution (PPM): ");
+  Serial.println(ppm);
+
+}
